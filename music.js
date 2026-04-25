afe7338baabf09bf9accecda4893a661815551fd
maksim12365 <122526290+maksim12365@users.noreply.github.com>
2026-04-25 13:50:32 +0800

    Update music.js

--- music.js
+++ music.js
@@ -5,7 +5,7 @@
 const MUSIC_CONFIG = {
   track: 'Ava',
   artist: 'Famy',
-  audioFile: 'https://cdn.jsdelivr.net/gh/maksim12365/10000arts@main/music/track.mp3',
+  audioFile: 'https://vk.com/away.php?to=https%3A%2F%2Fmy.mail.ru%2Fmusic%2Fsongs%2Fb2932239d01dce822ec3dd53362828c2',
   week: 1
 };
 
